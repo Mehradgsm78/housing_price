3d50115d7cefa8ccbd566244d979e5c3c1f064fd
Mehradgsm78 <mehradmorteza78@gmail.com>
2023-05-28 20:58:39 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use Kaggle's House Prices - Advance Regression Techniques data set. Train two models, a linear model and the',
+    author='mehrad',
+    license='',
+)
